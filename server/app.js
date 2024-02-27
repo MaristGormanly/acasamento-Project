c349d211d6ade202be69bf38818c860db7b0e574
acasamento <andrew.casamento1@marist.edu>
2024-02-27 13:35:26 -0500

    Milestone 1 Commit

--- server/app.js
+++ server/app.js
@@ -5,4 +5,6 @@ app.get('/', function (req, res) {
     res.sendFile('index.html', {root: './client/views'})
 })
 
+app.use(express.static('client/public'));
+
 app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
